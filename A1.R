31734d50a7b17495f64e3f614982fad11f59b355
mysunatislam <mysunatislam@gmail.com>
2026-05-25 00:58:27 +0600

    Merged datasets and reshaped transcriptomic data

---
+++ A1.R
@@ -0,0 +1,17 @@
+# Combine datasets
+combined_tbl <- left_join(
+  expr_tbl,
+  clinic_tbl,
+  by = c("ID" = "Patient_ID")
+)
+
+
+# Reshape data
+tidy_tbl <- combined_tbl %>%
+  pivot_longer(
+    c(Gene_A, Gene_B, Gene_C),
+    names_to = "Biomarker",
+    values_to = "Signal"
+  )
+
+
